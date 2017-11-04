4f81d64276bdef3d1fa985e3ab0000a169e6559c
Edvin <edvin.berg@comhem.com>
2017-11-04 22:01:15 +0100

    styling, fixed single post layout

--- single.php
+++ single.php
@@ -9,52 +9,6 @@
 
 get_header(); ?>
 
-<!--    <article id="post---><?php //the_ID(); ?><!--" --><?php //post_class(); ?><!-->
-<!--        --><?php //$url = wp_get_attachment_url(get_post_thumbnail_id(), 'thumbnail'); ?>
-<!---->
-<!--    </article>-->
-    <div class='description'>
-        <!-- description content -->
-        <header class="entry-header">
-            <?php
-
-                the_title('<span class="entry-title">', '</span>');
-
-//            if ('post' === get_post_type()) : ?>
-<!--                                <div class="entry-meta">-->
-<!--                                    --><?php //we_posted_on(); ?>
-<!--                                </div>-->
-<!--                --><?php
-//            endif; ?>
-
-
-
-            <div class="entry-content">
-                <?php
-                the_content(sprintf(
-                    wp_kses(
-                    /* translators: %s: Name of current post. Only visible to screen readers */
-                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'we'),
-                        array(
-                            'span' => array(
-                                'class' => array(),
-                            ),
-                        )
-                    ),
-                    get_the_title()
-                ));
-
-                wp_link_pages(array(
-                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'we'),
-                    'after' => '</div>',
-                ));
-                ?>
-            </div>
-        </header><!-- .entry-header -->
-
-        <!--        --><?php //the_ID(); ?>
-    </div>
-
 
 
     <div id="primary" class="content-area">
@@ -63,7 +17,7 @@ get_header(); ?>
             <?php
             while (have_posts()) : the_post();
 
-                get_template_part('template-parts/content', get_post_type());
+                get_template_part('template-parts/single-post-content', get_post_type());
 
 //                the_post_navigation();
 
