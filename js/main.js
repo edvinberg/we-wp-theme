00d4a12be39969c4923abb403afbe347242d8357
Edvin <edvin.berg@comhem.com>
2018-08-30 19:22:15 +0200

    basic style fixes

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
         $("#myvideo").prop('muted', false);
         $(function() {
             $('.scroll-down').click (function() {
-                $('html, body').animate({scrollTop: $('div.site-content').offset().top }, 'slow');
+                $('html, body').animate({scrollTop: $('div.site-content').offset().top-30 }, 'slow');
                 return false;
             });
         });
